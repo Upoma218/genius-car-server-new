d72fcd057c52604e9cc5b5ac6568239e6036202d
Resma Islam Upoma <upoma.cdsr@gmail.com>
2022-11-09 07:13:22 +0600

    final added

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express();
-const port = process.env.PORT || 5000;
 require('dotenv').config();
 
-// middleware
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
 app.use(cors());
 app.use(express.json());
-
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4cizlao.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri); 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('geniusCar').collection('services');
-        const orderCollection = client.db('geniusCar').collection('orders');
+        const orderCollection = client.db('geniusCar').collection('orders');  
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '5d'});
+            res.send({token});
+        })
 
         app.get('/services', async(req, res) => {
             const query = {};
@@ -38,26 +59,30 @@ async function run() {
 
         // orders api
 
-        app.get('/orders', async(req, res) => {
-            let query = {};
-            if(req.query.email){
-                query = {
-                    email: req.query.email,
-                };
-            }
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
             
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message: 'unauthorized access'})
+            }
+
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
             const cursor = orderCollection.find(query);
             const orders = await cursor.toArray();
             res.send(orders);
-        })
+        });
 
-        app.post('/orders', async(req, res) => {
+        app.post('/orders', verifyJWT, async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
-            console.log(result);
             res.send(result);
-        })
-        app.patch('/orders/:id', async(req, res) => {
+        });
+        app.patch('/orders/:id',verifyJWT, async(req, res) => {
             const id = req.params.id;
             const status = req.body.status;
             const query = {_id:ObjectId(id)};
@@ -69,7 +94,7 @@ async function run() {
             const result = await orderCollection.updateOne(query, updatedDoc);
             res.send(result);
         })
-        app.delete('/orders/:id', async(req, res) => {
+        app.delete('/orders/:id',verifyJWT, async(req, res) => {
             const id = req.params.id;
             const query = {_id:ObjectId(id)};
             const result = await orderCollection.deleteOne(query);
@@ -88,9 +113,6 @@ run().catch(err => console.error(err));
 app.get('/', (req, res) => {
     res.send('Genius car server is running');
 });
-
-
-
 
 app.listen(port, () => {
     console.log(`Genius car server running on ${port}`);
